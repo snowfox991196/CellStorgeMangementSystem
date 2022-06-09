928ffeb703083fafefdb1a54dad508900c3aa042
黑山老喵 <snowfox.0921@foxmail.com>
2022-06-09 13:41:13 +0800

    Update WIOrderOperations.php

--- csms/WIOrderOperations.php
+++ csms/WIOrderOperations.php
@@ -6,10 +6,10 @@ require 'DBHelper.php';
 
 $dbhelper = new DBHelper();
 
-$orderid = $_POST["orderid"];
-$ordersum = $_POST["operation"];
+$orderid = $_GET["orderid"];
+$ordersum = $_GET["operation"];
 
-$sql = "SELECT count(t.orderID) FROM csmsdb.wi_order t  where orderID = '".$orderid."'";
+$sql = "SELECT count(t.orderID) FROM csmsdb.wi_order t where orderID = '".$orderid."'";
 $data = $dbhelper->sqlSelect($sql);
 
 if ($data->num_rows > 0) {
